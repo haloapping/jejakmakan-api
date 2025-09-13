2be80c26a68582c6fea836cbb343325d48f839d1
haloapping <alfiyanto.kondolele@gmail.com>
2025-09-13 17:50:52 +0700

    fix: change conn string

--- config/config.go
+++ config/config.go
@@ -16,13 +16,13 @@ func DBConnStr(envName string) string {
 	}
 
 	connStr := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		"postgresql://%s:%s@%s:%s/%s?sslmode=%s",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
-		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_SSL_MODE"),
 	)
 
 	return connStr
